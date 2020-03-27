cb98a8ab183533f6fa3f823f430f0bbbb678ecfd
Victor Miranda <vmmelo1@gmail.com>
2020-03-27 00:50:49 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/*passa o parametro exact para evitar que caia sempre nessa rota ja que todos vao comecar com /. Assim garantimos que so cai nela se for exatamente apenas a / */}
+                <Route path={'/'} exact component={Logon}/>
+                <Route path={'/register'} component={Register}/>
+                <Route path={'/profile'} component={Profile}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
