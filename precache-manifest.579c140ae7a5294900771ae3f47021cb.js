39f5691986fde8e65838099e8af8c1bd49b10539
Victor Miranda <victor.miranda@idealizza.com.br>
2020-04-04 02:20:20 -0300

    Updates

---
+++ precache-manifest.579c140ae7a5294900771ae3f47021cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f41d1621dd5a28ddb5a538b4777b89f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42e9d23a64dac559f935",
+    "url": "./static/css/main.7b22bdb6.chunk.css"
+  },
+  {
+    "revision": "abd872bd66109feb1c70",
+    "url": "./static/js/2.4254f9c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4254f9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e9d23a64dac559f935",
+    "url": "./static/js/main.a772a089.chunk.js"
+  },
+  {
+    "revision": "e736dabb8ee024a7b980",
+    "url": "./static/js/runtime-main.3993e964.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "./static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "./static/media/logo.7eea718b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oministack-frontend-web/precache-manifest.4e79204d643f0e86fb0de664f27c68b8.js"
+  "./precache-manifest.579c140ae7a5294900771ae3f47021cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oministack-frontend-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
